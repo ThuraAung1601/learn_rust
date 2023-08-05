8425232dcae54bc7408401d24a5bb20a5f213d14
Thura Aung aka June <thuraaung.ai.mdy@gmail.com>
2023-08-05 22:12:01 +0700

    Add files via upload

---
+++ homework4/No1/fah2cel/src/main.rs
@@ -0,0 +1,86 @@
+// 1.1 Ordinary loop
+fn fahr_to_cel_v(v: &[f32]) -> Vec<f32> {
+    // Using reference value instead of cloning makes less computational cost
+    let mut x = Vec::new();
+    for _i in v {
+        // (5.0/9.0)*(fahr - 32.0)
+        x.push((5.0/9.0)*(_i - 32.0));
+    }
+    return x;
+}
+
+// 1.2 Recursion
+fn fahr_to_cel_v_recursion(v: &mut Vec<f32>, _i: usize) -> Vec<f32> {
+    // Modify the original vector v
+    if _i < v.len() {
+        v[_i] = (5.0 / 9.0) * (v[_i] - 32.0);
+        fahr_to_cel_v_recursion(v, _i+1);
+    }
+    return v.to_vec();
+}
+
+fn main(){
+    let v_args: Vec<String> = std::env::args().collect();
+    let mut v1 = Vec::new();
+    
+    // Parse it to numerical values
+    for i in 1..v_args.len() {
+        // if input is character, it will skip
+        match v_args[i].parse::<f32>() {
+            Ok(value) => v1.push(value),
+            Err(_) => {
+                eprintln!("Invalid input: \"{0}\" is not a valid input, skip \"{0}\"", v_args[i]);
+            }
+        }
+    }
+    // Input vector from user
+    println!("Fahr degrees: {:?}",v1);
+
+    // In ordinary loop, I used the reference value
+    println!("Celcius degrees w/ loop: {:?}",fahr_to_cel_v(&v1));
+
+    // In recursion, I used v1.clone() to prevent original value of v1
+    println!("Celcius degrees w/ recursion: {:?}",fahr_to_cel_v_recursion(&mut v1.clone(), 0));
+    
+    // To check the original vector is unchanged.
+    // println!("Fahr degrees: {:?}",v1);
+}
+
+// Test 1 without values
+#[test]
+fn test1_fahr_to_cel_v(){
+    let v1: Vec<f32> = vec![];
+    let expected: Vec<f32> = vec![];
+
+    // test with loop function
+    assert_eq!(fahr_to_cel_v(&v1),expected);
+
+    // test with recursive function
+    assert_eq!(fahr_to_cel_v_recursion(&mut v1.clone(), 0),expected);
+}
+
+// Test 2 with positive values
+#[test]
+fn test2_fahr_to_cel_v(){
+    let v1: Vec<f32> = vec![0.,20.,40.];
+    let expected: Vec<f32> = vec![-17.777779, -6.666667, 4.4444447];
+
+    // test with loop function
+    assert_eq!(fahr_to_cel_v(&v1),expected);
+
+    // test with recursive function
+    assert_eq!(fahr_to_cel_v_recursion(&mut v1.clone(), 0),expected);
+}
+
+// Test 3 with negative values
+#[test]
+fn test3_fahr_to_cel_v(){
+    let v1: Vec<f32> = vec![-0.,-20.,-40.];
+    let expected: Vec<f32> = vec![-17.777779, -28.88889, -40.0];
+
+    // test with loop function
+    assert_eq!(fahr_to_cel_v(&v1),expected);
+
+    // test with recursive function
+    assert_eq!(fahr_to_cel_v_recursion(&mut v1.clone(), 0),expected);
+}
---
+++ homework4/No2/grader/src/main.rs
@@ -0,0 +1,91 @@
+fn grade_checker(mark: f32) -> &'static str {
+    if mark >= 0.0 && mark <= 49.0 {return "Failed with F";}
+    else if mark >= 50.0 && mark <= 60.0 {return "D";}
+    else if mark >= 61.0 && mark <= 70.0 {return "C";}
+    else if mark >= 71.0 && mark <= 80.0 {return "B";}
+    else if mark >= 81.0 && mark <= 94.0 {return "A";}
+    else if mark >= 95.0 && mark <= 100.0 {return "Excellent with A+";}
+    else {return "Invalid score"} // this is the case < 0 or > 100
+}
+
+// No. 2.1
+fn make_grades(s: &[f32]) -> Vec<String> {
+    let mut x = Vec::new();
+    for _e in s {
+        // de-referencing element _e because input is f32 and _e is reference value &f32
+        x.push(grade_checker(*_e).to_string());
+    }
+    return x;
+}
+
+// No. 2.2
+fn make_grades_recursion(s: Vec<f32>, g: &mut Vec<String>, _i: usize) -> Vec<String> {
+   if _i < s.len() {
+        g.push(grade_checker(s[_i]).to_string());
+        make_grades_recursion(s, g, _i+1);
+   }
+    return g.clone();
+}
+
+fn main() {
+    let v_args: Vec<String> = std::env::args().collect();
+    let mut scores = Vec::new();
+
+    for _i in 1..v_args.len() {
+        // if input is character, it will skip
+        match v_args[_i].parse::<f32>() {
+            Ok(value) => scores.push(value),
+            Err(_) => {
+                eprintln!("Invalid input: \"{0}\" is not a valid input, skip \"{0}\"", v_args[_i]);
+            }
+        }
+    }
+    
+    // Input vector from user
+    println!("scores: {:?}",scores);
+
+    // In ordinary loop, I used the reference value
+    println!("grades w/ loop: {:?}",make_grades(&scores));
+
+    // In recursion, I have to initialize new vector with string
+    println!("grades w/ recursion: {:?}", make_grades_recursion(scores, &mut Vec::new(), 0));
+}
+
+// Test 1 without values
+#[test]
+fn test1_make_grades(){
+    let v1: Vec<f32> = vec![];
+    let expected: Vec<String> = vec![];
+
+    // test with loop function
+    assert_eq!(make_grades(&v1),expected);
+
+    // test with recursive function
+    assert_eq!(make_grades_recursion(v1, &mut Vec::new(), 0),expected);
+}
+
+// Test 2 with values
+#[test]
+fn test2_make_grades(){
+    let v1: Vec<f32> = vec![100., 80., 40.];
+    let expected: Vec<String> = vec!["Excellent with A+".to_string(), "B".to_string(), "Failed with F".to_string()];
+
+    // test with loop function
+    assert_eq!(make_grades(&v1),expected);
+
+    // test with recursive function
+    assert_eq!(make_grades_recursion(v1, &mut Vec::new(), 0),expected);
+}
+
+// Test 3 with invalid values
+#[test]
+fn test3_make_grades(){
+    let v1: Vec<f32> = vec![110., -80., 90.];
+    let expected: Vec<String> = vec!["Invalid score".to_string(), "Invalid score".to_string(), "A".to_string()];
+
+    // test with loop function
+    assert_eq!(make_grades(&v1),expected);
+
+    // test with recursive function
+    assert_eq!(make_grades_recursion(v1, &mut Vec::new(), 0),expected);
+}
