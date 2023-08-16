c9a219d2ad7a36eba43aa6ed42ea3587ff26e2dd
June <thuraaung.ai.mdy@gmail.com>
2023-08-16 20:01:42 +0700

    Update extract_quoted_word.rs

--- homework5/HW5/src/bin/extract_quoted_word.rs
+++ homework5/HW5/src/bin/extract_quoted_word.rs
@@ -1,3 +1,48 @@
-fn main() {
+fn extract_quoted_words(text: &str) -> Vec<String> {
+    let mut words = Vec::new();
+    let mut current_word = String::new();
+    let mut is_quoting = false;
+
+    for c in text.chars() {
+        match c {
+            '*' if !is_quoting => is_quoting = true,
+            '*' if is_quoting => {
+                if !current_word.is_empty() {
+                    words.push(current_word.clone());
+                }
+                current_word.clear();
+                is_quoting = false;
+            }
+            ' ' if is_quoting => {
+                if !current_word.is_empty() {
+                    words.push(current_word.clone());
+                }
+                current_word.clear();
+                is_quoting = false;
+            }
+            _ if is_quoting => current_word.push(c),
+            _ => {}
+        }
+    }
     
+    if !current_word.is_empty() {
+        words.push(current_word);
+    }
+
+    return words;
+}
+
+fn main() {
+    let input = "C *C++* *Python* Rust*";
+    println!("{:?}",extract_quoted_words(input));
+}
+
+#[test]
+fn test_extract_quoted_words() {
+    let expected_output: Vec<String> = vec![];
+    assert_eq!(extract_quoted_words(""), expected_output);
+    assert_eq!(
+    extract_quoted_words("*Python* Rust*"),
+        vec!["Python"] // "**", "*C++*", "*Python*"
+    );
 }
