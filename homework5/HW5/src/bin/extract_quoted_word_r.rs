f1c501372761935ed632982e89a3e10c663f565b
June <thuraaung.ai.mdy@gmail.com>
2023-08-17 21:30:51 +0700

    Update extract_quoted_word_r.rs

--- homework5/HW5/src/bin/extract_quoted_word_r.rs
+++ homework5/HW5/src/bin/extract_quoted_word_r.rs
@@ -1,49 +1,29 @@
-fn extract_quoted_words_r(text: &str) -> Vec<String> {
-    fn extract_recursive(text: &str, mut current_word: String, is_quoting: bool, words: &mut Vec<String>) {
-        if let Some(c) = text.chars().next() {
-            match c {
-                '*' if !is_quoting => extract_recursive(&text[1..], current_word, true, words),
-                '*' if is_quoting => {
-                    if !current_word.is_empty() {
-                        words.push(current_word.clone());
-                    }
-                    extract_recursive(&text[1..], String::new(), false, words);
-                }
-                ' ' if is_quoting => {
-                    if !current_word.is_empty() {
-                        words.push(current_word.clone());
-                    }
-                    extract_recursive(&text[1..], String::new(), false, words);
-                }
-                _ if is_quoting => {
-                    current_word.push(c);
-                    extract_recursive(&text[1..], current_word, is_quoting, words);
-                }
-                _ => extract_recursive(&text[1..], current_word, is_quoting, words),
+fn extract_quoted_words(qoute: &str) -> Vec<String> {
+    fn extract_recursive(qoute: &str, result: &mut Vec<String>) {
+        if let Some((word, rest)) = qoute.split_once(" ") {
+            let open_star = word.chars().next().unwrap_or('_');
+            let close_star = word.chars().rev().next().unwrap_or('_');
+
+            if open_star == '*' && close_star == '*' {
+                let (_, word) = word.split_at(1);
+                let (word, _) = word.split_at(word.len() - 1);
+
+                result.push(word.to_string());
             }
-        } else {
-            if !current_word.is_empty() {
-                words.push(current_word);
-            }
+
+            extract_recursive(rest, result);
         }
     }
 
-    let mut words = Vec::new();
-    extract_recursive(text, String::new(), false, &mut words);
-    words
+    let mut result = Vec::new();
+    extract_recursive(qoute, &mut result);
+
+    result
 }
 
 fn main() {
-    let input = "C *C++* *Python* Rust*";
-    println!("{:?}",extract_quoted_words_r(input));
-}
+    let qoute = "C ** *C++* *Java *Python* Rust*      ";
+    let result = extract_quoted_words(qoute);
 
-#[test]
-fn test_extract_quoted_words() {
-    let expected_output: Vec<String> = vec![];
-    assert_eq!(extract_quoted_words(""), expected_output);
-    assert_eq!(
-        extract_quoted_words_r("*Python* Rust*"),
-        vec!["Python"] // "**", "*C++*", "*Python*"
-    );
+    println!("Result of quote: {:?}", result);
 }
