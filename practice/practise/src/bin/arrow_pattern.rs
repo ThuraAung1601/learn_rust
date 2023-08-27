56abb6e79439b1bdb8b4b4788768342ab4f7fc84
June <thuraaung.ai.mdy@gmail.com>
2023-08-27 15:59:32 +0700

    Update arrow_pattern.rs

--- practice/practise/src/bin/arrow_pattern.rs
+++ practice/practise/src/bin/arrow_pattern.rs
@@ -28,7 +28,10 @@ fn arrow_pattern2(size: i32) -> String {
 
 
 fn main(){
-    let size = 3;
+    let args: Vec<String> = std::env::args().collect();
+    let s_arg = if args.len() < 2 {""} else {&args[1]};
+    let size: i32 = s_arg.parse().unwrap_or(0);
+
     println!("{}", arrow_pattern1(size));
     println!("{}", arrow_pattern2(size));
 }
