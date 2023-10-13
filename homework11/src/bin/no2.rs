fd60d4531ea7f5620f6fb703ea78a067daa69564
June <thuraaung.ai.mdy@gmail.com>
2023-10-13 09:50:38 +0000

    Update no2.rs

--- homework11/src/bin/no2.rs
+++ homework11/src/bin/no2.rs
@@ -23,7 +23,6 @@ fn word_counter(text: &str) -> usize {
 }
 
 impl Document {
-    // used impl for encapsulation
     fn make_document(file: &str, text: &str) -> Document {
         let mut para_vec = Vec::new();
         for l in text.split("\n\n") {
@@ -67,7 +66,7 @@ impl Document {
         ranked_docs
     }
 
-    fn generate_html(ranked: &[Document]) -> String {
+    fn generate_html_para(ranked: &[Document]) -> String {
         // Generate an HTML table
         let mut html_table = String::new();
         html_table.push_str("<table>\n");
@@ -77,6 +76,22 @@ impl Document {
             html_table.push_str("<tr>");
             html_table.push_str(&format!("<td>{}</td>", doc.name));
             html_table.push_str(&format!("<td>{:>02}</td>", doc.para_count));
+            html_table.push_str("</tr>\n");
+        }
+        html_table.push_str("</table>\n");
+        html_table
+    }
+
+    fn generate_html_word(ranked: &[Document]) -> String {
+        // Generate an HTML table
+        let mut html_table = String::new();
+        html_table.push_str("<table>\n");
+        html_table.push_str("<table border=\"1\" style=\"text-align: right;\">\n");
+        html_table.push_str("<tr><th>File</th><th>Count</th></tr>\n");
+        for doc in ranked {
+            html_table.push_str("<tr>");
+            html_table.push_str(&format!("<td>{}</td>", doc.name));
+            html_table.push_str(&format!("<td>{:>02}</td>", doc.word_count));
             html_table.push_str("</tr>\n");
         }
         html_table.push_str("</table>\n");
@@ -100,14 +115,14 @@ fn main() {
 
     // No 2.1
     let ranked_by_para_count = Document::rank_by_para_count(&unranked_docs);
-    let para_html_table = Document::generate_html(&ranked_by_para_count);
+    let para_html_table = Document::generate_html_para(&ranked_by_para_count);
     // Write the HTML file
     let mut output_file = File::create("doc_ranked_paracount.html").expect("Failed to create output file");
     write!(output_file, "{}", para_html_table).expect("Failed to write to output file");
 
     // No 2.2
     let ranked_by_word_count = Document::rank_by_word_count(&unranked_docs);
-    let word_html_table = Document::generate_html(&ranked_by_word_count);
+    let word_html_table = Document::generate_html_word(&ranked_by_word_count);
     // Write the HTML file
     let mut output_file = File::create("doc_ranked_wordcount.html").expect("Failed to create output file");
     write!(output_file, "{}", word_html_table).expect("Failed to write to output file");
